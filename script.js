e36dd4684d28eb6b68023a0e6b421727657bf931
jooprima <jooprima@gmail.com>
2019-12-14 17:14:36 +0700

    aturan main

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let jumlahKartu = 5;
+let kartuPertama = kartuKedua = 0;
 
 function buatAngka() {
     let angkaBerurut = [];
@@ -26,13 +27,38 @@ function persiapkanKartu(angkaAcak){
     let str = '';
 
     angkaAcak.forEach(function(i){
-        str += '<div class="kartu">'+
+        str += '<div class="kartu" nilai="'+i+'">'+
                     '<div class="belakang">' + i + '</div>'+
                     '<div class="Depan">jooDev</div>' + 
                 '</div>';
     });
 
     $('#game').append(str);
+}
+
+function pengendali(){
+    $('.kartu').on('click',function(){
+        $(this).addClass('buka');
+
+        if (kartuPertama == 0) {
+            kartuPertama = $(this).attr('nilai');
+            console.log(kartuPertama);
+            
+        }else{
+            kartuKedua = $(this).attr('nilai');
+
+            if (kartuPertama == kartuKedua) {
+                $('.buka').addClass('betul');
+                $('.betul').removeClass('buka');
+                kartuPertama = kartuKedua = 0;
+            }else{
+                kartuPertama = kartuKedua = 0;
+                $(this).one('transitionend',function(){
+                    $('.kartu').removeClass('buka');
+                });
+            }
+        }
+    })
 }
 
 $(document).ready(function(){
@@ -43,7 +69,9 @@ $(document).ready(function(){
 
     persiapkanKartu(angkaAcak);
 
+    pengendali();
+
     // console.log('ini adalah angka berurut : ' + angkaBerurut);
-    console.log('ini adalah angka acak : ' + angkaAcak);
+    // console.log('ini adalah angka acak : ' + angkaAcak);
     
 })
